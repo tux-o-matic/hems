a0ad5b2d810d39805fe6d76ad13fc43634d568ca
Benjamin Merot <bem.its@dsg.dk>
2018-05-14 19:43:44 +0200

    Working handler for object detection when requesting a JSON list result. Image result with boxes is WIP

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # hems
+
+## Running the API
+From within the same folder:
+```bash
+gunicorn -b localhost:8080 --worker-class gevent --threads 2 --worker-connections 200 app:app
+```
+
+## Calling the API
+To get the image back with boxes around detected objects:
+```bash
+curl -X POST http://127.0.0.1:8080/image --upload-file /path/to/image/to/scan.jpg -i
+```
+
+To get a JSON list of objects detected:
+```bash
+curl -H "Accept: application/json" -X POST http://127.0.0.1:8080/image --upload-file /path/to/image/to/scan.jpg -i
+```
+
+
+
+### Credits
+pyimagesearch.com for OpenCV examples
--- app.py
+++ app.py
@@ -1,34 +1,50 @@
-from flask import Flask, render_template
+from detector import Detector
+from flask import Flask, jsonify, render_template, request, send_file
 from flask_restful import Api
-from flask_restful_swagger import swagger
+# from flask_restful_swagger import swagger
 import configparser
-import cv2
-import imutils
-import numpy as np
 import socket
-import time
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 config = configparser.ConfigParser()
-
-url = config['server']['protocol'] + '://' + config['server']['hostname'] + ':' +  config['server']['port']
-api = swagger.docs(Api(application), apiVersion='0.1',
-                   basePath=url,
-                   resourcePath='/',
-                   produces=["application/json", "text/html"],
-                   api_spec_url='/api/spec', description='API Spec')
+config.read("config.ini")
+detector = Detector(config['models']['classes_json_path'], config['models']['model_path'],
+                    config['models']['prototxt_path'])
 
 
-@application.route("/")
+# url = config['server']['protocol'] + '://' + config['server']['hostname'] + ':' +  config['server']['port']
+# api = swagger.docs(Api(app), apiVersion='0.1',
+#                   basePath=url,
+#                   resourcePath='/',
+#                   produces=["application/json", "text/html"],
+#                   api_spec_url='/api/spec', description='API Spec')
+
+
+@app.route("/")
 def index():
     return render_template('index.html', title='Home')
 
 
-@application.route("/status")
+@app.route('/image', methods=['POST'])
+def post_image():
+    received_image = request.data
+
+    confidence_min = int(config['models']['confidence_min'])
+    if 'confidence_min' in request.args:
+        confidence_min = int(request.args.get(config['models']['confidence_min']))
+
+    if 'application/json' in request.accept_mimetypes:
+        return jsonify(detector.detect_in_image(received_image, confidence_min))
+    else:
+        return send_file(detector.detect_in_image(received_image, confidence_min, type_to_return='image'),
+                         mimetype='image/png')
+
+
+@app.route("/status")
 def status():
     return "I'm alive on " + str(socket.gethostbyname(socket.gethostname()))
 
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
