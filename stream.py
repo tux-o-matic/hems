3db1e19f2d4aac14d309d595c90afcdfc9c5a9b0
Benjamin Merot <4562092+tux-o-matic@users.noreply.github.com>
2020-01-22 21:51:00 +0100

    Reduced target FPS for smooth streaming
    Preventing raw 30 fps from over loading CPU, reduced computed frame and writing duplicated frames to output stream.
    Output to Gstreamer pipeline requires that OpenCV is built with support for it.

--- stream.py
+++ stream.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 import cv2
 import os
+import time
 
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "protocol_whitelist;file,rtp,udp"
 
@@ -18,37 +19,43 @@ def scan(args):
     print("[INFO] starting video stream...")
     cap = cv2.VideoCapture(os.path.join(args["working_dir"], 'stream.sdp'))
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(os.path.join(args["working_dir"], 'stream.avi'), fourcc, 30, (640, 480))
+    captureTime = time.time()
+
+    out_stream = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5001'
+    out = cv2.VideoWriter(out_stream, cv2.CAP_GSTREAMER,0, 20, (640,480), True)
 
     while(cap.isOpened()):
       ret, frame = cap.read()
       (h, w) = frame.shape[:2]
 
-      blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
-      net.setInput(blob)
-      detections = net.forward()
+      now = int(round(time.time() * 1000))
 
-      for i in np.arange(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+      if (int(now - captureTime)) >= 100:
+          blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+          net.setInput(blob)
+          detections = net.forward()
 
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+          for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > args["confidence"]:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-      #Local test
-      #cv2.imshow("Frame", frame)
-      #Write to file
-      out.write(frame)
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+
+          #Local test
+          #cv2.imshow("Frame", frame)
+          for i in range(3):
+            out.write(frame) #Duplicate frames,30 fps video
+          captureTime = int(round(time.time() * 1000))
+
+
 
       # if the `q` key was pressed, break from the loop
       key = cv2.waitKey(1) & 0xFF
