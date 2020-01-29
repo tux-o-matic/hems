6ab3b6576058538e6e80817dfb27424c794f4bb7
Benjamin Merot <4562092+tux-o-matic@users.noreply.github.com>
2020-01-29 07:12:50 +0100

    Switching to Gstreamer for input and output RTP stream

--- stream.py
+++ stream.py
@@ -17,12 +17,15 @@ def scan(args):
     net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
     print("[INFO] starting video stream...")
-    cap = cv2.VideoCapture(os.path.join(args["working_dir"], 'stream.sdp'))
+    #cap = cv2.VideoCapture(os.path.join(args["working_dir"], 'stream.sdp'))
+    cap = cv2.VideoCapture('udpsrc address=' + args['src_ip'] + ' port=' + args['src_port'] + ' caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
     captureTime = time.time()
 
-    out_stream = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5001'
-    out = cv2.VideoWriter(out_stream, cv2.CAP_GSTREAMER,0, 20, (640,480), True)
+    out_stream = 'appsrc ! queue ! videoconvert ! video/x-raw, width=640, height=480, framerate=10/1 ! queue ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! queue ! rtph264pay ! queue ! udpsink host=' + args['output_ip'] + ' port=' + args['output_port']
+    out = cv2.VideoWriter(out_stream, cv2.CAP_GSTREAMER, 0, 10, (640,480), True)
+    if not out.isOpened():
+        print("Output writer isn't opened")
 
     while(cap.isOpened()):
       ret, frame = cap.read()
@@ -53,6 +56,7 @@ def scan(args):
           #cv2.imshow("Frame", frame)
           for i in range(3):
             out.write(frame) #Duplicate frames,30 fps video
+            #print("Wrote frame to output stream")
           captureTime = int(round(time.time() * 1000))
 
 
@@ -71,6 +75,10 @@ if __name__ == "__main__":
     ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
     ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
     ap.add_argument("-c", "--confidence", type=float, default=0.2, help="minimum probability to filter weak detections")
+    ap.add_argument("-i", "--output_ip", default="127.0.0.1," help="IP address of the generated RTP stream")
+    ap.add_argument("-o", "--output_port", type=int, default=5001, help="UDP port of the generated RTP stream")
+    ap.add_argument("-s", "--src_ip", default="127.0.0.1," help="IP address of the source RTP stream")
+    ap.add_argument("-u", "--src_port", type=int, default=5000, help="UDP port of the source RTP stream")
     ap.add_argument("-w", "--working_dir", required=True, help="Directory where stream.sdp and output is written to")
     args = vars(ap.parse_args())
 
