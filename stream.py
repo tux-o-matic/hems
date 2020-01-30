87a4dcb94bc45a0c75342c55cdf9da94a7dd0a7c
Benjamin Merot <4562092+tux-o-matic@users.noreply.github.com>
2020-01-30 15:18:28 +0100

    Various syntax fixes

--- stream.py
+++ stream.py
@@ -16,11 +16,11 @@ def scan(args):
     net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
     print("[INFO] starting video stream...")
-    cap = cv2.VideoCapture('udpsrc address=' + args['src_ip'] + ' port=' + args['src_port'] + ' caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture('udpsrc multicast-group=' + args['src_ip'] + ' port=' + str(args['src_port']) + ' auto-multicast=true caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
     captureTime = time.time()
 
-    out_stream = 'appsrc ! queue ! videoconvert ! video/x-raw, width=' + args['width'] + ', height=' + args['height'] + ', framerate=10/1 ! queue ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! queue ! rtph264pay ! queue ! udpsink host=' + args['output_ip'] + ' port=' + args['output_port']
+    out_stream = 'appsrc ! queue ! videoconvert ! video/x-raw, width=' + str(args['width']) + ', height=' + str(args['height']) + ', framerate=10/1 ! queue ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! queue ! rtph264pay ! queue ! udpsink host=' + args['output_ip'] + ' port=' + str(args['output_port'])
     out = cv2.VideoWriter(out_stream, cv2.CAP_GSTREAMER, 0, 10, (args['width'], args['height']), True)
     if not out.isOpened():
         print("Output writer isn't opened")
@@ -73,13 +73,12 @@ if __name__ == "__main__":
     ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
     ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
     ap.add_argument("-c", "--confidence", type=float, default=0.2, help="minimum probability to filter weak detections")
-    ap.add_argument("-i", "--output_ip", default="127.0.0.1," help="IP address of the generated RTP stream")
+    ap.add_argument("-i", "--output_ip", default="127.0.0.1", help="IP address of the generated RTP stream")
     ap.add_argument("-o", "--output_port", type=int, default=5001, help="UDP port of the generated RTP stream")
-    ap.add_argument("-s", "--src_ip", default="127.0.0.1," help="IP address of the source RTP stream")
+    ap.add_argument("-s", "--src_ip", default="127.0.0.1", help="IP address of the source RTP stream")
     ap.add_argument("-u", "--src_port", type=int, default=5000, help="UDP port of the source RTP stream")
-    ap.add_argument("-d", "--working_dir", required=True, help="Directory where stream.sdp and output is written to")
     ap.add_argument("-w", "--width", type=int, default=1280, help="Pixel width of the video stream")
-    ap.add_argument("-h", "--height", type=int, default=720, help="Pixel height of the video stream")
+    ap.add_argument("-t", "--height", type=int, default=720, help="Pixel height of the video stream")
     args = vars(ap.parse_args())
 
     scan(args)
